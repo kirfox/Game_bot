298c0fc8a3069383bf80fe388da9039895bf3bc8
Kirill <muxin.kirill2011@yandex.ru>
2020-08-13 18:28:31 +0300

    refactor: i did the sixth hard task

--- script.js
+++ script.js
@@ -6,35 +6,60 @@ let isNumber = function(n) {
 };
 
 let guess = function(){
+
+    let attempt = 10;
     // random numeric
     let num = Math.floor(Math.random() * 100) + 1;
-
+    console.log(num);
     let guessNum = prompt('Угадай число от 1 до 100');
-    
+    attempt--;
     let guessFun = function(){
-    
+        
+        
         //checking a random number
-        if (guessNum > num && isNumber(guessNum)) {
-            guessNum = prompt('Загаданное число меньше \nУгадай число от 1 до 100');
-        } else if (guessNum < num && isNumber(guessNum)){
-            guessNum = prompt('Загаданное число больше \nУгадай число от 1 до 100');
+        if (guessNum > num && isNumber(guessNum) && attempt !== 0) {
+            guessNum = prompt('Загаданное число меньше, осталось попыток - '+ attempt );
+            attempt--;
+            console.log(attempt);
+        } else if (guessNum < num && isNumber(guessNum) && attempt !== 0){
+            guessNum = prompt('Загаданное число больше, осталось попыток - '+ attempt );
+            attempt--;
+            console.log(attempt);
         } else if (guessNum === null){
-        } else if (!isNumber(guessNum)){
+        } else if (!isNumber(guessNum) ){
             alert('Введи число!');
-            guessNum = prompt('Угадай число от 1 до 100');
+            guessNum = prompt('осталось попыток - '+ attempt);
+            attempt--;
+            console.log(attempt);
         } else  {
-            alert('Вы вышли из игры!');
+            if (attempt <= 0) {
+                let result = confirm('Попытки закончились, хотите сыграть еще?'); 
+                if (result) {
+                    guess();
+                } else {
+                    alert('Вы вышли из игры! До встречи!');
+                    return;
+                }  
+            }
         }
 
         //recursion: check for victory
         if (guessNum == num) {
-            alert('Победа, Вы угадали число');
+            let won = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+            if (won) {
+                guess();
+            } else {
+                alert('Вы вышли из игры! До встречи!');
+                return;
+            }  
         } else if (guessNum === null){
-            alert('Вы вышли из игры!');
+            alert('Вы вышли из игры! До встречи!');
         } else {
             return guessFun();
         }
     };
+
+    
 
     return guessFun();
 };
